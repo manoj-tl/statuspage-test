51d9c39a0ff7a5f57c5e89bc60832902164a9d26
Manoj Nadella <manojn@zillowgroup.com>
2020-03-17 14:53:29 -0700

    Adding statuspage plugin

--- trulia.tf
+++ trulia.tf
@@ -1,17 +1,17 @@
-module "trulia_home_page" {
+module "comp_trulia_hp" {
   source                = "./component/"
   component_name        = "Home Page"
   component_description = "Trulia Homepage"
 }
 
-module "trulia_pdp" {
+module "comp_trulia_pdp" {
   source                = "./component/"
   component_name        = "Product detail page"
   component_description = "Trulia Product Detail Page"
 }
 
-module "Truila_CG" {
+module "comp_group_truila" {
   source               = "./component_group/"
   component_group_name = "Trulia TF"
-  component_id         = [module.trulia_pdp.component_id, module.trulia_home_page.component_id]
+  component_id         = [module.comp_trulia_hp.component_id, module.comp_trulia_pdp.component_id]
 }
