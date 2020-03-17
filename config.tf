0c1b37e2b3e572a006b556f82d02524465b583d8
Manoj Nadella <manojn@zillowgroup.com>
2020-03-17 13:07:27 -0700

    Adding statuspage plugin

--- config.tf
+++ config.tf
@@ -1,5 +1,5 @@
-//provider "statuspage" {
-//  token = "ca3c11ad-4bfd-4cbc-b633-61051e08e6f6"
-//}
+provider "statuspage" {
+  token = "ca3c11ad-4bfd-4cbc-b633-61051e08e6f6"
+}
 
-provider "aws" {}
+//provider "aws" {}
